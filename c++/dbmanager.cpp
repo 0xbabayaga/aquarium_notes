ace016475a3a2ef14af5427a55d81b1c348af900
0xbabayaga <webmaster@aquabox.by>
2020-05-06 19:29:11 +0300

    SQL tables refactored

--- c++/dbmanager.cpp
+++ c++/dbmanager.cpp
@@ -166,7 +166,7 @@ void DBManager::onGuiPersonalParamStateChanged(int paramId, bool en)
 bool DBManager::getParamsList()
 {
     bool res = false;
-    QSqlQuery query("SELECT * FROM DICTTABLE");
+    QSqlQuery query("SELECT * FROM DICT_TABLE");
     ParamObj *obj = nullptr;
     int i = 0;
 
@@ -190,8 +190,8 @@ bool DBManager::getParamsList()
 bool DBManager::getParamsList(QString tankId)
 {
     bool res = false;
-    QSqlQuery query("SELECT * FROM DICTTABLE");
-    QSqlQuery queryPersonal("SELECT * FROM PERSONALPARAMTABLE WHERE TANK_ID = '"+tankId+"'");
+    QSqlQuery query("SELECT * FROM DICT_TABLE");
+    QSqlQuery queryPersonal("SELECT * FROM PERSONAL_PARAM_TABLE WHERE TANK_ID = '"+tankId+"'");
     ParamObj *obj = nullptr;
     QMap<int, bool> mapPersonal;
     int i = 0;
@@ -231,7 +231,7 @@ bool DBManager::getLatestParams()
     bool found = false;
     LastDataParamRecObj *recObj = nullptr;
     QList<int> smpIdList;
-    QSqlQuery query0("SELECT SMP_ID FROM LOGTABLE WHERE TANK_ID='"+currentTankSelected()->tankId()+"' ORDER BY SMP_ID DESC");
+    QSqlQuery query0("SELECT SMP_ID FROM HISTORY_VALUE_TABLE WHERE TANK_ID='"+currentTankSelected()->tankId()+"' ORDER BY SMP_ID DESC");
 
     smpIdList.clear();
 
@@ -261,7 +261,7 @@ bool DBManager::getLatestParams()
 
     if (smpIdList.size() > 0)
     {
-        QSqlQuery query("SELECT * FROM LOGTABLE "
+        QSqlQuery query("SELECT * FROM HISTORY_VALUE_TABLE "
                         "WHERE SMP_ID = '"+QString::number(smpIdList.at(0))+"'");
 
         while (query.next())
@@ -277,7 +277,7 @@ bool DBManager::getLatestParams()
 
         if (smpIdList.size() > 1)
         {
-            QSqlQuery query1("SELECT * FROM LOGTABLE "
+            QSqlQuery query1("SELECT * FROM HISTORY_VALUE_TABLE "
                                     "WHERE SMP_ID='"+QString::number(smpIdList.at(1))+"'");
 
             while (query1.next())
@@ -320,7 +320,7 @@ int DBManager::getLastSmpId()
 {
     int id = 0;
 
-    QSqlQuery query("SELECT MAX(ID) FROM LOGTABLE");
+    QSqlQuery query("SELECT MAX(ID) FROM HISTORY_VALUE_TABLE");
 
     if(query.next())
         id = query.value(0).toInt();
@@ -330,7 +330,7 @@ int DBManager::getLastSmpId()
 
 bool DBManager::getCurrentUser()
 {
-    QSqlQuery query("SELECT * FROM UTABLE");
+    QSqlQuery query("SELECT * FROM USER_TABLE");
 
     while (query.next())
     {
@@ -346,7 +346,7 @@ bool DBManager::getUserTanksList()
 {
     bool res = false;
     TankObj *obj = nullptr;
-    QSqlQuery query("SELECT * FROM TANKSTABLE WHERE MAN_ID='"+curSelectedObjs.user->man_id+"'");
+    QSqlQuery query("SELECT * FROM TANKS_TABLE WHERE MAN_ID='"+curSelectedObjs.user->man_id+"'");
 
     curSelectedObjs.listOfUserTanks.clear();
 
@@ -370,7 +370,7 @@ bool DBManager::createUser(QString uname, QString upass, QString phone, QString 
         QSqlQuery query;
         bool res = false;
 
-        query.prepare("INSERT INTO UTABLE (MAN_ID, UNAME, UPASS, SELECTED, STATUS, PHONE, EMAIL, DATE_CREATE, DATE_EDIT) "
+        query.prepare("INSERT INTO USER_TABLE (MAN_ID, UNAME, UPASS, SELECTED, STATUS, PHONE, EMAIL, DATE_CREATE, DATE_EDIT) "
                       "VALUES (:man_id, :uname, :upass, :selected, :status, :phone, :email, :date_create, :date_edit)");
 
         query.bindValue(":man_id", randId());
@@ -403,7 +403,7 @@ bool DBManager::createTank(QString name, QString manId, int type, int l, int w, 
         QSqlQuery query;
         QString tankId = randId();
 
-        query.prepare("INSERT INTO TANKSTABLE (TANK_ID, MAN_ID, TYPE, NAME, STATUS, L, W, H, DATE_CREATE, DATE_EDIT) "
+        query.prepare("INSERT INTO TANKS_TABLE (TANK_ID, MAN_ID, TYPE, NAME, STATUS, L, W, H, DATE_CREATE, DATE_EDIT) "
                       "VALUES (:tank_id, :man_id, :type, :name, :status, :l, :w, :h, :date_create, :date_edit)");
 
         query.bindValue(":tank_id", tankId);
@@ -444,7 +444,7 @@ bool DBManager::createTankDefaultParamSet(QString tankId)
         {
             obj = (ParamObj*) paramsGuiList.at(i);
 
-            query.prepare("INSERT INTO PERSONALPARAMTABLE (PARAM_ID, TANK_ID, ENABLED) "
+            query.prepare("INSERT INTO PERSONAL_PARAM_TABLE (PARAM_ID, TANK_ID, ENABLED) "
                           "VALUES (:param_id, :tank_id, :enabled)");
 
             query.bindValue(":param_id", obj->paramId());
@@ -468,7 +468,7 @@ bool DBManager::addParamRecord(int smpId, int paramId, double value)
     bool res = false;
     TankObj *tank = (TankObj*) curSelectedObjs.listOfUserTanks.at(curSelectedObjs.tankIdx);
 
-    query.prepare("INSERT INTO LOGTABLE (SMP_ID, TANK_ID, PARAM_ID, VALUE, TIMESTAMP) "
+    query.prepare("INSERT INTO HISTORY_VALUE_TABLE (SMP_ID, TANK_ID, PARAM_ID, VALUE, TIMESTAMP) "
                   "VALUES (:smp_id, :tank_id, :param_id, :value, :tm)");
 
     query.bindValue(":smp_id", smpId);
@@ -490,7 +490,7 @@ bool DBManager::editPersonalParamState(QString tankId, int paramId, bool en)
     QSqlQuery query;
     bool res = false;
 
-    res = query.exec("UPDATE PERSONALPARAMTABLE SET ENABLED = '"+QString::number(en)+"' WHERE "
+    res = query.exec("UPDATE PERSONAL_PARAM_TABLE SET ENABLED = '"+QString::number(en)+"' WHERE "
                      "TANK_ID = '" +tankId+ "' AND PARAM_ID = '" +QString::number(paramId)+"'");
 
     if (res == false)
@@ -522,7 +522,7 @@ bool DBManager::initDB()
 {
     QSqlQuery query;
 
-    query.exec("create table UTABLE "
+    query.exec("create table USER_TABLE "
                 "(MAN_ID varchar(16), "
                 "UNAME varchar(64), "
                 "UPASS varchar(128), "
@@ -530,19 +530,19 @@ bool DBManager::initDB()
                 "STATUS integer, "
                 "PHONE varchar(16), "
                 "EMAIL varchar(64), "
-                "AVATAR_IMG text, "
+                "AVATAR_IMG blob, "
                 "DATE_CREATE integer, "
                 "DATE_EDIT integer )");
 
     qDebug() << query.lastError();
 
-    query.exec("create table TANKSTABLE "
+    query.exec("create table TANKS_TABLE "
                 "(TANK_ID varchar(16), "
                 "MAN_ID varchar(16), "
                 "TYPE integer, "
                 "NAME varchar(64), "
                 "DESC text, "
-                "IMG text, "
+                "IMG blob, "
                 "STATUS integer, "
                 "L integer, "
                 "W integer, "
@@ -552,7 +552,7 @@ bool DBManager::initDB()
 
     qDebug() << query.lastError();
 
-    query.exec("create table LOGTABLE "
+    query.exec("create table HISTORY_VALUE_TABLE "
                 "(ID integer PRIMARY KEY AUTOINCREMENT, "
                 "SMP_ID integer, "
                 "TANK_ID varchar(16), "
@@ -562,66 +562,124 @@ bool DBManager::initDB()
 
     qDebug() << query.lastError();
 
-    query.exec("create table DICTTABLE "
+    query.exec("create table HISTORY_NOTES_TABLE "
+                "(SMP_ID integer, "
+                "TANK_ID varchar(16), "
+                "TEXT text, "
+                "IMAGEDATA blob, "
+                "IMAGELINK varchar(64),"
+                "TIMESTAMP integer)");
+
+    qDebug() << query.lastError();
+
+    query.exec("create table DICT_TABLE "
                 "(PARAM_ID integer, "
                 "SHORT_NAME varchar(8), "
                 "FULL_NAME varchar(32), "
-                "UNIT_NAME varchar(8))");
+                "UNIT_NAME varchar(8), "
+                "MIN_LIMIT text,"
+                "MAX_LIMIT text)");
 
     qDebug() << query.lastError();
 
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (0, '-', '-', '-')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (1, 'Temp.', 'Temperature', '°C')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (2, 'Sal.', 'Salinity', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (3, 'Ca', 'Calcium', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (4, 'pH', 'pH', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (5, 'kH', 'kH', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (6, 'gH', 'gH', 'dKh')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (7, 'Po4', 'Phosphates', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (8, 'No2', 'Nitrite', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (9, 'No3', 'Nitrate', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (10, 'Nh3', 'Ammonia', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (11, 'Co2', 'Carbon', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (12, 'O2', 'Oxigen', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (13, 'Mg', 'Magnesium', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (14, 'Si', 'Silicates', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (15, 'K', 'Potassium', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (16, 'I', 'Iodine', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (17, 'Sr', 'Strontium', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (18, 'Fe', 'Ferrum', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (19, 'Cu', 'Cuprum', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (20, 'B', 'Boron', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (21, 'Mo', 'Molybdenum', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (22, 'Cl', 'Clorine', 'ppm')");
-    query.exec("INSERT INTO DICTTABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME) "
-               "VALUES (23, 'Orp', 'ORP', 'ppm')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (0, '-', '-', '-', '', '')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (1, 'Temp.', 'Temperature', '°C', "
+               "'25.0 25.0 25.0 25.0 25.0 25.0 25.0 25.0',"
+               "'29.0 29.0 29.0 29.0 29.0 29.0 29.0 29.0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (2, 'Sal.', 'Salinity', 'ppm',"
+               "'31.0 33.0 33.0 33.0 0 0 0 0',"
+               "'33.0 35.0 35.0 35.0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (3, 'Ca', 'Calcium', 'ppm',"
+               "'350 350 350 350 0 0 0 0',"
+               "'500 500 500 500 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (4, 'pH', 'pH', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (5, 'kH', 'kH', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (6, 'gH', 'gH', 'dKh',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (7, 'Po4', 'Phosphates', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (8, 'No2', 'Nitrite', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (9, 'No3', 'Nitrate', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (10, 'Nh3', 'Ammonia', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (11, 'Co2', 'Carbon', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (12, 'O2', 'Oxigen', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (13, 'Mg', 'Magnesium', 'ppm', "
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (14, 'Si', 'Silicates', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (15, 'K', 'Potassium', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (16, 'I', 'Iodine', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (17, 'Sr', 'Strontium', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (18, 'Fe', 'Ferrum', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (19, 'Cu', 'Cuprum', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (20, 'B', 'Boron', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (21, 'Mo', 'Molybdenum', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (22, 'Cl', 'Clorine', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
+    query.exec("INSERT INTO DICT_TABLE (PARAM_ID, SHORT_NAME, FULL_NAME, UNIT_NAME, MIN_LIMIT, MAX_LIMIT) "
+               "VALUES (23, 'Orp', 'ORP', 'ppm',"
+               "'0 0 0 0 0 0 0 0',"
+               "'0 0 0 0 0 0 0 0')");
 
     qDebug() << query.lastError();
 
-    query.exec("create table PERSONALPARAMTABLE "
+    query.exec("create table PERSONAL_PARAM_TABLE "
                 "(PARAM_ID integer, "
                 "TANK_ID varchar(16), "
                 "ENABLED integer)");
